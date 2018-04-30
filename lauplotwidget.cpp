432d9c5474bd326e7baa74063935e1be621a94fd
Daniel L. Lau <dllau@engr.uky.edu>
2018-04-30 11:17:59 -0400

    Added semilog scale plots.

--- lauplotwidget.cpp
+++ lauplotwidget.cpp
@@ -12,8 +12,6 @@ LAUPlotWidget::LAUPlotWidget(Style stl, QWidget *parent) : QWidget(parent), styl
         // SET AXES FOR SHOWING DENSITIES
         plot->xAxis->setLabel("Time [n]");
         plot->xAxis2->setVisible(true);
-        plot->xAxis->setRange(0, windowSize);
-        plot->xAxis2->setRange(0, windowSize);
 
         plot->yAxis->setLabel("Raw Audio Signal");
         plot->yAxis2->setVisible(true);
@@ -23,10 +21,16 @@ LAUPlotWidget::LAUPlotWidget(Style stl, QWidget *parent) : QWidget(parent), styl
         plot->yAxis2->setRange(-0.2, 0.2);
     } else if (style == StylePSD) {
         // SET AXES FOR SHOWING DENSITIES
-        plot->xAxis->setLabel("Frequency [k]");
+        plot->xAxis->setLabel("Frequency (Hz)");
         plot->xAxis2->setVisible(true);
         plot->xAxis->setRange(0, windowSize);
         plot->xAxis2->setRange(0, windowSize);
+        plot->xAxis->setScaleType(QCPAxis::stLogarithmic);
+        plot->xAxis2->setScaleType(QCPAxis::stLogarithmic);
+
+        QSharedPointer<QCPAxisTickerLog> logXTicker(new QCPAxisTickerLog);
+        plot->xAxis->setTicker(logXTicker);
+        plot->xAxis2->setTicker(logXTicker);
 
         plot->yAxis->setLabel("PSD Audio Signal");
         plot->yAxis2->setVisible(true);
@@ -48,11 +52,17 @@ void LAUPlotWidget::onUpdateBuffer(float *buffer, int samples)
     // BOTH CAN HOLD THE INCOMING SAMPLES
     if (x.size() != samples) {
         x.resize(samples);
-        for (int n = 0; n < samples; n++) {
-            x[n] = (double)n;
+        if (style == StyleRaw) {
+            for (int n = 0; n < samples; n++) {
+                x[n] = (double)n;
+            }
+        } else if (style == StylePSD) {
+            for (int n = 0; n < samples; n++) {
+                x[n] = 44100.0 * (double)n / (double)samples;
+            }
         }
-        plot->xAxis->setRange(0, samples);
-        plot->xAxis2->setRange(0, samples);
+        plot->xAxis->setRange(0, x[samples - 1]);
+        plot->xAxis2->setRange(0, x[samples - 1]);
     }
 
     if (y.size() != samples) {
--- tunergui.cpp
+++ tunergui.cpp
@@ -1,7 +1,7 @@
 #include "tunergui.h"
 #include "ui_tunergui.h"
 
-TunerGUI::TunerGUI(QWidget *parent) : QWidget(parent), ui(new Ui::TunerGUI), object(NULL)
+TunerGUI::TunerGUI(QWidget *parent) : QWidget(parent), ui(new Ui::TunerGUI), object(NULL), fftObject(NULL), rawWidget(NULL), psdWidget(NULL)
 {
     ui->setupUi(this);
 
@@ -13,22 +13,35 @@ TunerGUI::TunerGUI(QWidget *parent) : QWidget(parent), ui(new Ui::TunerGUI), obj
     fftObject->setWindowSize(1024);
 
     rawWidget = new LAUPlotWidget(LAUPlotWidget::StyleRaw);
+    rawWidget->setMinimumHeight(200);
     rawWidget->setWindowSize(1024);
+    ui->rawAudioGroupBox->setAutoFillBackground(true);
     ui->rawAudioGroupBox->layout()->addWidget(rawWidget);
 
     psdWidget = new LAUPlotWidget(LAUPlotWidget::StylePSD);
+    psdWidget->setMinimumHeight(200);
     psdWidget->setWindowSize(1024);
     ui->psdAudioGroupBox->layout()->addWidget(psdWidget);
 
+    // CONNECT ALL OUR OBJECTS IN ORDER OF EXECUTION
     connect(object, SIGNAL(emitUpdateBuffer(float *, int)), rawWidget, SLOT(onUpdateBuffer(float *, int)));
     connect(rawWidget, SIGNAL(emitUpdateBuffer(float *, int)), fftObject, SLOT(onUpdateBuffer(float *, int)));
     connect(fftObject, SIGNAL(emitUpdateBuffer(float *, int)), psdWidget, SLOT(onUpdateBuffer(float *, int)));
     connect(psdWidget, SIGNAL(emitUpdateBuffer(float *, int)), this, SLOT(onUpdateBuffer(float *, int)));
+
+    // CALL THESE SLOTS SO THAT WE KNOW OUR OBJECTS MATCH THE CURRENT USER INTERFACE SETTINGS
+    on_windowFunctionComboBox_currentIndexChanged(ui->windowFunctionComboBox->currentText());
+    on_windowSizeComboBox_currentIndexChanged(ui->windowSizeComboBox->currentText());
 }
 
 TunerGUI::~TunerGUI()
 {
+    // DELETE THE UI AND ANY WIDGETS INSIDE ITS LAYOUTS LIKE THE PLOTTING WIDGETS
     delete ui;
+
+    // MAKE SURE WE DELETE THE AUDIO OBJECT LAST SINCE IT CONTROLS THE DATA BUFFERS
+    delete fftObject;
+    delete object;
 }
 
 void TunerGUI::on_windowFunctionComboBox_currentIndexChanged(QString string)
